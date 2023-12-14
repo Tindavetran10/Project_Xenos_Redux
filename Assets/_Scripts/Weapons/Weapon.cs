640e0116a0d59bad9c001bb493a00fbbd07a9195
Tindavetran10 <gade.tintran@gmail.com>
2023-12-15 03:51:17 +0700

    Update Attack Animation and Function
    Player Attack Animation can run through all attacks
    After a small amount of time, the combo will reset to the first attack

--- Assets/_Scripts/Weapons/Weapon.cs
+++ Assets/_Scripts/Weapons/Weapon.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using Timer = _Scripts.Utilities.Timer;
+
 
 namespace _Scripts.Weapons
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] private int numberOfAttacks;
+        [SerializeField] private float attackCounterResetCoolDown;
+
+        private int CurrentAttackCounter
+        {
+            get => _currentAttackCounter;
+            set => _currentAttackCounter = value >= numberOfAttacks ? 0 : value;
+        }
+        
         public event Action OnExit;
         
         private Animator _anim;
@@ -13,28 +24,55 @@ namespace _Scripts.Weapons
         private static readonly int Active = Animator.StringToHash("active");
 
         private AnimationEventHandler _eventHandler;
-        
+
+        private int _currentAttackCounter;
+        private static readonly int Counter = Animator.StringToHash("counter");
+
+        private Timer _attackCounterResetTimer;
+
         public void Awake()
         {
             _baseGameObject = transform.Find("Base").GameObject();
             _anim = _baseGameObject.GetComponent<Animator>();
             
             _eventHandler = _baseGameObject.GetComponent<AnimationEventHandler>();
+
+            _attackCounterResetTimer = new Timer(attackCounterResetCoolDown);
         }
+
+        private void Update() => _attackCounterResetTimer.Tick();
+
+        private void ResetAttackCounter() => CurrentAttackCounter = 0;
         
         public void Enter()
         {
             print($"{transform.name} enter");
+            
+            _attackCounterResetTimer.StopTimer();
+            
             _anim.SetBool(Active, true);
+            _anim.SetInteger(Counter, CurrentAttackCounter);
         }
         
         private void Exit()
         {
             _anim.SetBool(Active, false);
+
+            CurrentAttackCounter++;
+            _attackCounterResetTimer.StartTimer();
             OnExit?.Invoke();
         }
 
-        private void OnEnable() => _eventHandler.OnFinish += Exit;
-        private void OnDisable() => _eventHandler.OnFinish -= Exit;
+        private void OnEnable()
+        {
+            _eventHandler.OnFinish += Exit;
+            _attackCounterResetTimer.OnTimerDone += ResetAttackCounter;
+        }
+
+        private void OnDisable()
+        {
+            _eventHandler.OnFinish -= Exit;
+            _attackCounterResetTimer.OnTimerDone -= ResetAttackCounter;
+        }
     }
 }
